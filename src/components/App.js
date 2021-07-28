0f101c851c2baf2cc70d71c855de8d501bd55853
John <jyk595@gmail.com>
2021-07-28 16:14:28 -0700

    Delvierable 1 to show all cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+// [X] When the app starts, I can see all listings.
+// [ ] I can "favorite" and "unfavorite" a listing on the frontend by clicking the star icon. This feature doesn't need backend persistence.
+// [ ] I can remove a listing from the page by clicking the trash can icon. This change should be persisted in the backend.
+// [ ] I can search for listings by their name.
+
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listingsData, setListingsData] = useState([]);
+
+  useEffect(()=>{
+    fetch(`http://localhost:6001/listings`)
+    .then(res=>res.json())
+    .then(setListingsData);
+  },[])
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listingsData={listingsData} />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,15 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ listingsData }) {
+  const renderListings = listingsData.map((individualListing)=>{
+    return <ListingCard individualListing={individualListing} key={individualListing.id} />
+  })
+  
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {renderListings}
       </ul>
     </main>
   );
